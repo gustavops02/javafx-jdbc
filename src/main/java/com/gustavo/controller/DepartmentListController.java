4e9919c97d32bddc561c06ac4edb02e659d81609
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2024-01-09 21:15:40 -0300

    Dialog - modal design

--- src/main/java/com/gustavo/controller/DepartmentListController.java
+++ src/main/java/com/gustavo/controller/DepartmentListController.java
@@ -3,16 +3,25 @@ package com.gustavo.controller;
 import com.gustavo.App;
 import com.gustavo.model.entities.Department;
 import com.gustavo.model.services.DepartmentService;
+import com.gustavo.utils.Alerts;
+import com.gustavo.utils.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -36,8 +45,9 @@ public class DepartmentListController implements Initializable {
     private ObservableList<Department> obsList;
 
     @FXML
-    public void onBtnNewAction() {
-        System.out.println("BTNNEWACTION");
+    public void onBtnNewAction(ActionEvent event) {
+        Stage parent = Utils.currentStage(event);
+        createDialogForm("DepartmentForm", parent);
     }
 
     public void setDepartmentService(DepartmentService service) {
@@ -65,5 +75,23 @@ public class DepartmentListController implements Initializable {
         List<Department> departmentList = service.findAll();
         obsList = FXCollections.observableArrayList(departmentList);
         tableViewDepartment.setItems(obsList);
+    }
+
+    private void createDialogForm(String fileName, Stage parentStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(App.class.getResource(fileName + ".fxml"));
+            Pane pane = loader.load();
+
+            Stage dialog = new Stage();
+            dialog.setTitle("Registro de departamento");
+            dialog.setScene(new Scene(pane));
+            dialog.setResizable(false);
+            dialog.initOwner(parentStage);
+            dialog.initModality(Modality.WINDOW_MODAL);
+            dialog.showAndWait();
+
+        } catch (IOException e) {
+            Alerts.showAlert("Erro", null, "Erro ao carregar o formulário", Alert.AlertType.ERROR);
+        }
     }
 }
